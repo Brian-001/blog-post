069ed9684cd2a3c672a5e2fe0f4a46346cdf6a89
Brian <karanjabrian001@gmail.com>
2025-02-25 18:46:56 +0300

    Added conditional taailwindcss formating in Home.jsx

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home({posts}){
     return (
         <>
             <h1 className="text-blue-500 mt-10 flex items-center justify-center text-center">Hello</h1>
-            {posts.map(post => (
+            {posts.data.map(post => (
                 <div key={post.id} className="p-4 border-b border-gray-200">
                     <div className="text-sm text-slate-600">
                         <span>Posted on: </span>
@@ -15,6 +15,26 @@ export default function Home({posts}){
                     <p className="font-medium">{post.body}</p>
                 </div>
             ))}
+
+            <div className="py-12 px-4">
+                {posts.links.map((link) =>(
+                    link.url ?
+                    <Link
+                        key={link.key}
+                        href={link.url}
+                        dangerouslySetInnerHTML={{__html: link.label}}
+                        className={`p-1 mx-1 ${link.active ? "text-blue-500 font-bold" : ""}`}
+                    />
+                    :
+                    <span
+                        key={link.key}
+                        dangerouslySetInnerHTML={{__html: link.label}}
+                        className="px-1 mx-1 text-slate-300"
+                    >
+
+                    </span>
+                ))}
+            </div>
             
         </>
     )
